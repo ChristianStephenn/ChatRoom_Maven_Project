35fd7a0ef589c0b451378ce8472bdb6331a90136
Luzarios <63917571+Luzarios@users.noreply.github.com>
2020-06-09 11:32:34 +0200

    Add JavaDoc

--- src/main/java/Presentation/Model/User.java
+++ src/main/java/Presentation/Model/User.java
@@ -2,25 +2,71 @@ package Presentation.Model;
 
 import java.io.Serializable;
 
+/**
+  * <b>User est une classe qui permet d'obtenir toutes les informations nécessaires lorsqu'il se connecte sur Chat Room.</b>
+  * <p>
+  * User contient :
+  * <ul>
+  * <li>Nom</li>
+  * <li>Ip</li>
+  * <li>Port</li>
+  *</ul>
+  */
 public class User implements Serializable {
+    
+    /**
+     * Le nom du User.
+     */
     private String name;
+    
+    /**
+     * L'ip du User.
+     */
     private String ip;
+    
+    /**
+     * Le port du User.
+     */
     private int port;
 
+    /**
+     * Constructeur User.
+     *
+     * @param name
+     *            Le nom du User.
+     * @param ip
+     *            L'ip du User.
+     * @param port
+     *            Le port du User.
+     */
     public User(String name, String ip, int port) {
         this.name = name;
         this.ip = ip;
         this.port = port;
     }
-
+    
+    /**
+     * Retourne le nom du User.
+     *
+     * @return Le nom correspondant, chaîne de caractères.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Retourne l'ip du User.
+     *
+     * @return L'ip correspondant, entier.
+     */
     public String getIp() {
         return ip;
     }
-
+    /**
+     * Retourne le port du User.
+     *
+     * @return Le port correspondant, entier.
+     */
     public int getPort() {
         return port;
     }
