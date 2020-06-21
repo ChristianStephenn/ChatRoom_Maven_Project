1493bcfe342626c2b5055a9ada7ca02abeae8397
Luzarios <63917571+Luzarios@users.noreply.github.com>
2020-06-21 12:43:01 +0200

    add JavaDoc

--- src/main/java/ChatRoom.java
+++ src/main/java/ChatRoom.java
@@ -1,6 +1,10 @@
 import Presentation.Constants;
 import Presentation.Controller.Server;
 
+/**
+  * <b>ChatRoom est une classe qui permet de lancer le serveur.</b>
+  */
+
 public class ChatRoom {
 
     public static void main(String[] args) {
